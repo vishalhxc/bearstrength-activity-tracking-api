cdc67fed404dc453837571f9f63da7f74ab6d9cf
vishalhxc <vishalhxc@gmail.com>
2020-04-17 17:05:13 -0500

    initial commit

---
+++ src/main/kotlin/com/bearstrength/activitytrackingapi/ActivityTrackingApiApplication.kt
@@ -0,0 +1,11 @@
+package com.bearstrength.activitytrackingapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActivityTrackingApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActivityTrackingApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/bearstrength/activitytrackingapi/ActivityTrackingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bearstrength.activitytrackingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivityTrackingApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
